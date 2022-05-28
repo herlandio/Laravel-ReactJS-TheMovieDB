ccc524dcace06925a3bd8fa3fd8dff66d67604ec
herlandio <herlandio7@gmail.com>
2022-05-28 14:43:20 -0300

    fix api

--- frontthemovieorg/src/Apis/ApiTheMovieDB.js
+++ frontthemovieorg/src/Apis/ApiTheMovieDB.js
@@ -3,17 +3,17 @@ import React from "react";
 class ApiTheMovieDB extends React.Component {
 
     async fetchJsonApi() {
-        const response = await fetch('http://api-themovieorg.herokuapp.com/api');
+        const response = await fetch('https://api-themovieorg.herokuapp.com/api');
         return await response.json();
     }
 
     async fetchJsonMovieById(id) {
-        const response = await fetch(`http://api-themovieorg.herokuapp.com/movieById/${id}`);
+        const response = await fetch(`https://api-themovieorg.herokuapp.com/movieById/${id}`);
         return await response.json();
     }
 
     async fetchJsonQuery(query) {
-        const response = await fetch(`http://api-themovieorg.herokuapp.com/search/${query}`);
+        const response = await fetch(`https://api-themovieorg.herokuapp.com/search/${query}`);
         return await response.json();
     }
 }
--- frontthemovieorg/src/Apis/fetchApiTheMovieDB.js
+++ frontthemovieorg/src/Apis/fetchApiTheMovieDB.js
@@ -1,14 +1,14 @@
 export async function fetchJsonApi() {
-    const response = await fetch('http://api-themovieorg.herokuapp.com/api/start');
+    const response = await fetch('https://api-themovieorg.herokuapp.com/api/start');
     return await response.json();
 }
 
 export async function fetchJsonMovieById(id) {
-    const response = await fetch(`http://api-themovieorg.herokuapp.com/api/movieById/${id}`);
+    const response = await fetch(`https://api-themovieorg.herokuapp.com/api/movieById/${id}`);
     return await response.json();
 }
 
 export async function fetchJsonQuery(query) {
-    const response = await fetch(`http://api-themovieorg.herokuapp.com/api/search/${query}`);
+    const response = await fetch(`https://api-themovieorg.herokuapp.com/api/search/${query}`);
     return await response.json();
 }
